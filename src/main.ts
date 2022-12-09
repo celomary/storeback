c7038c84247417a35c945a2f0cf3111b374c6eba
Mohamed Elomary <elomarymohamed72@gmail.com>
2022-12-09 16:45:21 +0100

    o

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,13 @@
 import { ApolloServer } from 'apollo-server';
-import express from 'express';
+
 import typeDefs from './schema';
 import resolvers from './resolvers';
-
-const app  = express();
 
 const server = new ApolloServer({
     typeDefs: typeDefs,
     resolvers: resolvers
 });
 
-server.applyMiddleware({ app });
-
-app.listen(433, '0.0.0.0', () => {
-    console.log('Server is running on port 433');
+server.listen(433, '0.0.0.0').then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
 });
-
